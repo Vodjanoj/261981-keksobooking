df640de121275f9ea74a57ce462bc02ec8b20c9c
Vodjanoj <vladimirs.vodjanojs@gmail.com>
2018-05-02 12:41:52 +0300

    Закончил работу с DOM

--- js/map.js
+++ js/map.js
@@ -92,28 +92,24 @@ for (var i = 0; i <= 7; i++) {
                       features: finalOffList, description: OFFER_DESCRIPTION, photos: OFFER_PHOTOS}, location: {x: finalOffLocX, y: finalOffLocY}});
 }  
 
-console.log(offers_around);
-
 var userDialog = document.querySelector('.map');
 userDialog.classList.remove('map--faded');
  
-
-debugger; 
-
- 
-var mapPinElement = document.querySelector('.map__pin');
+var mapPinElement = document.querySelector('.map__pins');
+var correctionY = 40; // смещение метки по Y чтобы на координату указывал именно острый конец метки
 
 var fragment = document.createDocumentFragment();
 
-for (var i = 1; i < 8; i++) {
+for (var i = 0; i < offers_around.length; i++) {
  
 var button = document.createElement('button');
-button.setAttribute('style', offers_around[i].author);
+button.setAttribute('style','left: ' + offers_around[i].location.x + 'px' + '; ' + 'top: ' + (offers_around[i].location.y - correctionY) + 'px');
 button.setAttribute('class', 'map__pin');
 
 var img = document.createElement('img'); 
 button.appendChild(img);
  
+img.setAttribute('src', offers_around[i].author.avatar); 
 img.setAttribute('width', '40');
 img.setAttribute('height', '40');
 img.draggable = false;
@@ -123,11 +119,53 @@ fragment.appendChild(button);
 
 mapPinElement.appendChild(fragment);
 
+var similarOfferTemplate = document.querySelector('template').content;
 
+var offerElement = similarOfferTemplate.cloneNode(true);
+
+offerElement.querySelector('h3').textContent = offers_around[0].offer.title;
+offerElement.querySelector('small').textContent = offers_around[0].offer.adress;
+offerElement.querySelector('.popup__price').textContent = offers_around[0].offer.price + '&#x20bd;/ночь';
+offerElement.querySelector('h4').textContent = offers_around[0].offer.type;
+offerElement.querySelectorAll('p')[2].textContent = offers_around[0].offer.rooms + ' комнат для ' + offers_around[0].offer.guests + ' гостей';
+offerElement.querySelectorAll('p')[3].textContent = 'Заезд после ' + offers_around[0].offer.checkin + ', ' + 'выезд до ' + offers_around[0].offer.checkout;
+
+for (var i = 0; i < offerElement.querySelector('.popup__features').children.length; i++) {
+    
+  offerElement.querySelector('.popup__features').children[i].textContent = offers_around[0].offer.features[i];
+} 
+
+offerElement.querySelectorAll('p')[4].textContent = offers_around[0].offer.description;
+offerElement.querySelector('.popup__pictures').children[0].setAttribute('src', offers_around[0].author.avatar);
+
+var mapWhole = document.querySelector('.map');
+
+mapWhole.insertBefore(offerElement, mapWhole.children[1]);
+
+// var beforeTheElement = document.body.children[1].children[0].children[1]; // maps-filter-container
+
+// console.log(document.body.children[1].children[0]); // map (надо делать в маp конечно)
+
+//  document.body.children[1].children[0].insertBefore(offerElement, beforeTheElement);
+
+//document.querySelector('.map__filters-container').insertBefore(offerElement, document.querySelector('.map__filters-container'));
+ 
+
+ // for (var i = 0; i < offers_around[0].offer.features.length; i++) {
+    
+  // offerElement.querySelector('.feature.feature--wifi').textContent = offers_around[0].offer.features[i];
+
+//} 
+
+// console.log(offerElement.querySelector('.feature.feature--wifi')); 
+
+// offerElement.querySelector('.popup__features').children[4].classList.contains('feature--elevator'); можно проверять
 
 
 
   // offers_around.push({author: {avatar: avoidDublicates(AUTHOR_AVATAR[Math.floor(Math.random() * AUTHOR_AVATAR.length)], offers_around)}});
+
+  // offerElement.querySelector('.feature.feature--wifi').textContent = 'Привееет'; 
  
  /*
  for (var j = 0; j <= 4; i++) {
